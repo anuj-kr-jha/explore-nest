8bbb4ca956e0dff90e731f1a111d18d2faca4cd5
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2023-08-06 08:23:17 +0530

    alias root controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service.js';
 
-@Controller()
+@Controller('')
 export class AppController {
   #appService: AppService;
   constructor(appService: AppService) {
